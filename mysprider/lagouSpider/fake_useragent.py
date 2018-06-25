e4657208fab744c6ab060f761cb7e816229dd7e7
cmzzcm <1104319927@qq.com>
2018-06-25 21:03:17 +0800

    web

---
+++ mysprider/lagouSpider/fake_useragent.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+def UserAgent():
+    ua_list = [
+            'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 ',
+            '(KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36',
+            'Mozilla/5.0 (compatible; Baiduspider/2.0; +http://www.baidu.com/search/spider.html)',
+            'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)',
+            'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
+            'Mozilla/5.0 (compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm)',
+        ]
